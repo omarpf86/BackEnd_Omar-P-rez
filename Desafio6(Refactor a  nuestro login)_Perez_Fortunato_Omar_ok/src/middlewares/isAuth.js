b534c9d7e42067eb007fc1c5ab8f0c1c0f2eac4b
omarpf86 <omarpf86@gmail.com>
2024-08-13 18:56:23 -0300

    Delete Desafio6(Refactor a  nuestro login)_Perez_Fortunato_Omar_ok directory

--- Desafio6(Refactor a  nuestro login)_Perez_Fortunato_Omar_ok/src/middlewares/isAuth.js
+++
@@ -1,6 +0,0 @@
-export const isAuth = (req, res, next) => {
-    if (req.session.passport && req.session.passport.user) {
-        if (req.isAuthenticated()) return next();
-    }
-    res.status(403).json({ msg: "No Autorizado" });
-};
